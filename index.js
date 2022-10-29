2cfe6f42326d1f367215c7f210ff38e490234471
siranjeeviajith <siranjeeviajith8@gmail.com>
2022-10-29 20:13:20 +0530

    Made proper changes to validate status of CI workflow

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  // `payload` input defined in action metadata file
-  const payload = core.getInput('payload');
-  console.log(`Hello ${payload}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const jsonPayload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${jsonPayload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+/*Inputs*/
+const token = core.getInput("token");
+const owner = core.getInput("repo");
+const workflow_id = core.getInput("workflow_id");
+const branch = core.getInput("branch");
+
+const fetchWorkFlow = () => {
+  return github.getOctokit(token).rest.actions.listWorkflowRuns({
+    owner,
+    repo,
+    workflow_id,
+    branch,
+  });
+};
+const run = async () => {
+  try {
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+
+    let workflow;
+    let status, conclusion;
+    do {
+      const { data } = await fetchWorkFlow();
+      workflow = data?.workflow_runs?.[0];
+      status = workflow?.status;
+      conclusion = workflow?.conclusion;
+      console.log(`Workflow status: ${status}`);
+      setTimeout(() => {}, 1000 * 1);
+    } while (status !== "completed");
+    console.log(`Workflow conclusion: ${conclusion}`);
+    if (conclusion !== "success")
+      core.setFailed(
+        `Workflow run failed : status${status} conclusion:${conclusion}`
+      );
+    core.setOutput('result', conclusion);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+run();
